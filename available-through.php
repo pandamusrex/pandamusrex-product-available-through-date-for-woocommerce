ad497af8ac3d0cfa272b89acec2c072a9f889c89
PandamusRex <pandamusrex@gmail.com>
2025-07-26 08:53:18 -0700

    Add license to plugin header

--- available-through.php
+++ available-through.php
@@ -6,15 +6,17 @@
  * Description: Disable adding a product to the cart after a date
  * Author: PandamusRex
  * Author URI: https://www.github.com/pandamusrex/
- * Requires at least: 4.0
- * Tested up to: 4.0
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 6.4.3
+ * Tested up to: 6.8.2
  *
  * Text Domain: available-through
  * Domain Path: /lang/
  *
  * @package WordPress
  * @author PandamusRex
- * @since 1.0.0
+ * @since 1.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
